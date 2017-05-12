1a93bb56f730af7108ae53ae4b4d4d42d694b9e6
Alex Speasmaker <alex.speasmaker@starlab.io>
2017-05-12 11:16:48 -0500

    Added tracking logic and desktracker config file to store fitbit address

--- desktracker.py
+++ desktracker.py
@@ -2,30 +2,44 @@ import time
 import datetime
 from bluetooth.ble import DiscoveryService
 
-file = open( "myaddr.txt", "r" )
+
+SLEEP_INTERVAL = 5
+ADDRESS_FILE = "./desktracker.cfg"
 
 target_name = "Charge 2"
-target_address = str( file.read() )
+target_address = str.strip( file.read() )
+
+file = open( "./desktracker.cfg", "r" )
 
 print "target_address: ", target_address
+print "target_name: ", target_name
 
 #This is only for low enery bluetooth devices
 service = DiscoveryService()
 
 while True:
 
+    device_present = False
+
     #Do this every 5 minutes
     devices = service.discover(2)
 
     for address, name in devices.items():
-        print "found address, ", address
-        if address == target_address:
-            print "addresses equal"
-        else:
-            print address,"!=",target_address
+        address = str.strip( address )
+        name = str.strip( name )
+
+        if address == target_address     \
+            and target_name == name:
+            
+            device_present = True
+
+    if device_present == True:
+        print "Device Present"
+    else:
+        print "Device away"
 
 #    print "You are away", datetime.datetime.now()
-    time.sleep(5)
+    time.sleep( SLEEP_INTERVAL )
 
 
 
